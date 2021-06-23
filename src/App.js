03b52ea6cad71776978ceaebe3cbfaa17b6b278c
moete <moetez.boubakri@esprit.tn>
2021-06-23 14:39:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import styled from "styled-components";
+import Header from "./components/Header";
+const Welcome = React.lazy(() => import("./pages/Welcome"));
+const Products = React.lazy(() => import("./pages/Products"));
+const ProductDetails = React.lazy(() => import("./pages/ProductDetails"));
 
 function App() {
+  const connectedUser =undefined;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <AppFrame className="App">
+        <BrowserRouter basename="/">
+          <Suspense fallback={<p>...Loading page please wait</p>}>
+            <Switch>
+              <Route
+                path="/welcome"
+                render={(props) => <Welcome {...props} />}
+              ></Route>
+              <Route
+                path="/products"
+                render={(props) => <Products {...props} />}
+              ></Route>
+              <Route
+                path="/product/:name"
+                render={(props) => <ProductDetails {...props} />}
+              ></Route>
+              <Route exact render={() => <p>Default rendered page!</p>}></Route>
+            </Switch>
+          </Suspense>
+        </BrowserRouter>
+      </AppFrame>
+    </>
   );
 }
-
+const AppFrame = styled.div`
+text-align: center;
+display: flex;
+flex-direction: column;
+`;
 export default App;
---
+++ src/components/Product.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+export default class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { product: props.product, updated: 0 };
+        this.addPrice = this.addPrice.bind(this);
+    }
+    render() {
+        return <ProductFrame>
+            <ProductImageWrapper>
+                <ProductImage src={this.props.product.img} ></ProductImage>
+            </ProductImageWrapper>
+            <ProductInfoWrapper>
+                <span>
+                    <a href={"/product/" + this.state.product.name}>
+                        {this.state.product.name}
+                    </a>
+                </span>
+                <span>
+                    {this.state.product.price}{" "}
+                    {/* {Number(this.state.product.price) > 2 ? "expensive" : "Promo"} */
+                    }
+                    {Number(this.state.product.price) > 2 && "expensive"}
+                </span>
+                <button onClick={this.addPrice}>Add 0.1</button>
+            </ProductInfoWrapper>
+        </ProductFrame>
+    }
+    addPrice(e) {
+        e.preventDefault();
+        this.setState((oldState) => ({
+            product: {
+                ...oldState.product, price: Number(oldState.product.price)
+                    + 0.1
+            },
+            updated: oldState.updated + 1,
+        }));
+    }
+}
+
+const ProductFrame = styled.div`
+border-radius: 25px;
+min-height: 150px;
+min-width: 150px;
+background-color: rgb(110, 110, 110, 0.7);
+margin: 10px;
+`;
+const ProductImageWrapper = styled.div`
+margin: 5px;
+max-width: 150px;
+`;
+const ProductImage = styled.img`
+width: 100%;
+height: 100%;
+border-radius: 25px;
+`;
+const ProductInfoWrapper = styled.div`
+margin-top: auto;
+margin-bottom: 5px;
+display: flex;
+flex-direction: column;
+& > span {
+text-align: center;
+}
+`;
